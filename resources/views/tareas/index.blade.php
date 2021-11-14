0fd039461f23a6d43cbe34daddafe0d2a2f6ea4f
yinsaire <ysaire.191ci32@istta.edu.pe>
2021-11-13 21:59:59 -0500

    Se agrega vistas

---
+++ resources/views/tareas/index.blade.php
@@ -0,0 +1,17 @@
+@extends('app')
+
+@section('content')
+
+<div class="container w-25 border p-4 mt-4">
+   <form>
+
+      <div class="mb-3">
+         <label for="title" class="form-label">Título de la tarea</label>
+         <input type="text" name="title" class="form-control">
+      </div>
+
+      <button type="submit" class="btn btn-primary">Crear tarea</button>
+
+   </form>
+</div>
+@endsection
